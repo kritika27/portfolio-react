7149644d0e1cc46947348110f9118a083965650d
Kritika Srivastava <kritikasri27@gmail.com>
2022-07-16 11:26:30 +0530

    Added routes for different pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import Navbar from "./components/Navbar"
+import Skills from "./pages/Skills"
+import Projects from "./pages/Projects"
+import Blog from "./pages/Blog"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </>
   );
 }
-
-export default App;
